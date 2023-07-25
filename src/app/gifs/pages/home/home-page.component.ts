0bdcb6877dd8d4e9aee27dd9207b8c569f69a063
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-07-25 14:05:04 +0200

    Diseño de los Gifs Components y prepacion tarea de CardList 84

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
 
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from '../gifs/services/components/sidebar/sidebar.component';
-
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    SidebarComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule
