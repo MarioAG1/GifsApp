4663f2058f083ae9f99f8b0495e3bc23cee6c8e4
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-07-25 13:29:39 +0200

    Arreglo de los directorios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarComponent } from '../gifs/services/components/sidebar/sidebar.component';
+
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
   ],
   imports: [
     CommonModule
