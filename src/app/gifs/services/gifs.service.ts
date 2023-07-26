9176729d8de86e05bc568bd6fd0317744d9abe8f
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-07-26 21:08:20 +0200

    Realizar una peticion HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
   private _tagsHistory: string[] = []
+  private apiKey: string = "xvCPmpylb87lU4q42onwNAAidChHpaj4"
+  private serviceUrl: string = "https://api.giphy.com/v1/gifs"
+
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     //Importante cada vez que hagas un get hacer un ... para poder mostrar los datos siempre que tenga un private
@@ -22,11 +27,23 @@ export class GifsService {
   }
 
   //Busqueda
-  public searchTag(tag: string): void {
+  public async searchTag(tag: string): Promise<void> {
     if (tag.length === 0) {
       return
     }
     this.organizeHistory(tag)
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+      .subscribe(resp => {
+        console.log(resp)
+      })
+
   }
 
 }
