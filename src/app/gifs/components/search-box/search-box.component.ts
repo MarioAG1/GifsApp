84aff2d2171604d15b6c688d2b72103999f0dc1b
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-07-26 14:34:40 +0200

    Utilizacion de ViewChild para la refencias en html

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,4 +7,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class SearchBoxComponent {
 
+  //Obtener la referncia local es un arreglo
+  @ViewChild("txtTagInput")
+
+  //La exclamacion es para decir que siempre existira ese valor
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  // searchTag(newtag: string)
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value
+
+    console.log(newTag);
+  }
 }
