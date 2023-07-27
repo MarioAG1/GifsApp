a04a70a303f7a114ae09e2dadf962f1d78478f75
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-07-27 13:48:53 +0200

    Tarea realizada relacionado con los botones

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { GifsService } from 'src/app/gifs/services/gifs.service';
+
 
 @Component({
   selector: 'shared-sidebar',
@@ -10,8 +11,13 @@ export class SidebarComponent {
 
   constructor(private gifsService: GifsService) { }
 
-  get tags() {
+  get tags(): string[] {
     return [...this.gifsService.tagsHistory];
   }
+
+  public searchTag(tag: string): void {
+    this.gifsService.searchTag(tag)
+  }
+
 
 }
