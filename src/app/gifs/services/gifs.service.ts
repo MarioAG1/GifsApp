da72f8841d30c19d12de3a01af529308062cd148
MarioAG1 <marioalvarezgonzalez21@gmail.com>
2023-07-27 22:08:57 +0200

    Crear y leer LocalStorage para mantener los datos

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,23 +11,44 @@ export class GifsService {
   private apiKey: string = "xvCPmpylb87lU4q42onwNAAidChHpaj4"
   private serviceUrl: string = "https://api.giphy.com/v1/gifs"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStrorage()
+    this.searchTag(this._tagsHistory[0])
+  }
+
 
   get tagsHistory() {
     //Importante cada vez que hagas un get hacer un ... para poder mostrar los datos siempre que tenga un private
     return [...this._tagsHistory]
   }
 
+  //Guardar en el LocalStorage
+  private saveLocalStorage(): void {
+    localStorage.setItem("history", JSON.stringify(this._tagsHistory))
+  }
+
+  private loadLocalStrorage():void {
+    if(!localStorage.getItem) {
+      //Si no hay informacion
+      return
+    }
+    //Obligacion de que siempre hay un dato ! si no, no deja
+    this._tagsHistory = JSON.parse(localStorage.getItem("history")!);
+
+  }
+
   //Comprobar que ya no existe nada repetido y el maximo de 10 solicitudes a la izquierda
   private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
-    if (this.tagsHistory.includes(tag)) {
+    if (this._tagsHistory.includes(tag)) {
       this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag)
     }
 
     this._tagsHistory.unshift(tag)
     this._tagsHistory = this._tagsHistory.splice(0, 10)
+    this.saveLocalStorage();
   }
+
 
   //Busqueda
   public async searchTag(tag: string): Promise<void> {
